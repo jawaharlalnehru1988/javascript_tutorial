35d77b0be0df05cf9fae5e3ae8d0c5bcadaed7b1
nehru00123@gmail.com <nehru00123@gmail.com>
2024-11-06 08:40:15 +0530

    scopes are added

---
+++ scopes.js
@@ -0,0 +1,41 @@
+
+
+// console.log(firstName, middleName, lastName);
+
+function addName(){
+  
+    if(true){
+        var firstName = "Jawahar";
+        let middleName = "Lal";
+        const lastName = "Nehru";
+        console.log(firstName, middleName, lastName);
+    }
+    console.log(firstName);
+}
+
+// if(false){
+//     console.log(firstName, middleName, lastName);
+// } else if( false){
+
+// } else {
+//     console.log(firstName, middleName, lastName);
+// }
+
+// for(let i = 0; i < 10; i++){
+//     console.log(firstName, middleName, lastName);
+// }
+
+addName();
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+while (condition) {
+    
+}
