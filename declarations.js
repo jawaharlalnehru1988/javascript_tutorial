8efad7b645b627e8232eee100b54700b4f837f41
nehru00123@gmail.com <nehru00123@gmail.com>
2024-11-07 06:55:27 +0530

    declarations and assigments

---
+++ declarations.js
@@ -0,0 +1,23 @@
+var firstName;
+let middleName;
+const lastName = "Nehru";
+console.log(firstName, middleName, lastName);
+
+var firstName;
+
+if(true){
+    const lastName = "Nehru";
+    console.log('lastName :', lastName);
+    let middleName;
+    console.log('middleName :', middleName);
+}
+
+var studentName = "Jawaharlal";
+studentName = "Nehru";
+console.log('studentName :', studentName);
+let teacherName = "Jawaharlal Nehru";
+teacherName = "Krishna Das";
+console.log('teacherName :', teacherName);
+const principalName = "Ganesh Das"; 
+// principalName = "Jawaharlal Nehru";
+console.log('principalName :', principalName);
