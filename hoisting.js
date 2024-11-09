db4ad75b18544488bb4882d6b04f78355aa8dfb3
nehru00123@gmail.com <nehru00123@gmail.com>
2024-11-09 19:23:36 +0530

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,15 @@
+addName();
+// console.log(firstName);
+// console.log(middleName);
+// console.log(lastName);
+
+var firstName  = "Jawahar";
+
+let middleName = "Lal";
+
+const lastName = "Nehru";
+
+function addName() {    
+    var goodName = "Keshavaraj";
+    console.log(goodName);
+}
