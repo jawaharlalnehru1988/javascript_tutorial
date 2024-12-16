3f48f62484c4dacf0b6d71065f92229d9e1732de
nehru00123@gmail.com <nehru00123@gmail.com>
2024-12-16 10:51:04 +0530

    date manipulation is added\

---
+++ dateManupulation.js
@@ -0,0 +1,91 @@
+//Date Manupulation
+//To print the current date
+
+// const today = new Date();
+// console.log(today);
+
+// //To print the current year
+// console.log(today.getFullYear());
+
+// //To print the current month
+// console.log(today.getMonth()+1);
+
+// //To print the current day
+// console.log(today.getDate());
+
+// //To print the current day of the week
+// console.log(today.getDay()+1);
+
+// //To print the current hour
+// console.log(today.getHours());
+
+// //To print the current minute
+// console.log(today.getMinutes());
+
+// //To print the current second
+// console.log(today.getSeconds());
+
+// //To print the current millisecond
+// console.log(today.getMilliseconds());
+
+// //To print the current time
+// console.log(today.getTime());
+
+// //To print the current timezone offset
+// console.log(today.getTimezoneOffset());
+
+// //To print the current date and time
+// console.log(today.toDateString());
+
+// //To print the current date and time
+// console.log(today.toLocaleDateString());
+
+// //To print the current date and time
+// console.log(today.toLocaleString());
+
+// //To print the current date and time
+// console.log(today.toISOString());
+
+// //To print the current date and time
+// console.log(today.toUTCString());
+
+// //To print the current date and time in custom format
+// console.log(today.toISOString().slice(0, 10));
+
+// //To print the current date and time in custom format
+// console.log(today.toISOString().slice(0, 10).split("-").reverse().join("-"));
+
+// //To print the current date and time in custom format
+
+// const formattedDate = today.toLocaleDateString('en-IN', {
+//     year: 'numeric',
+//     month: '2-digit',
+//     day: '2-digit',
+// })
+
+// console.log(formattedDate);
+
+//-----------------------------------------------------------------
+
+// pass value into date
+
+// const date = new Date("2023-01-01");
+// console.log(date);
+
+// set date from string
+
+// const pastDate = new Date(0);
+// console.log(pastDate);
+
+// const futureDate = new Date(86400000);
+// console.log(futureDate);
+
+// const customDate = new Date(2023, 0, 1);
+// console.log(customDate);
+
+// const customDate2 = new Date(2023, 0, 1, 12, 34, 56);
+// console.log(customDate2);
+
+// const customDate3 = new Date(2023, 0, 1, 12, 34, 56, 789);
+// console.log(customDate3);
+
---
+++ stringmanupulation.js
@@ -0,0 +1,182 @@
+//String manipulation in javascript
+
+// length
+
+// let name = "Krishna";
+
+// console.log(name.length);
+
+//--------------------------------------------------------------
+
+//charAt()
+
+// let myName = "Krishna";
+
+// console.log(myName.charAt(0));
+
+// console.log(myName.charAt(1));
+
+// console.log(myName.charAt(2));
+
+//--------------------------------------------------------------
+
+//charCodeAt()
+
+// let myName = "Krishna";
+
+// console.log(myName.charCodeAt(0));
+
+// console.log(myName.charCodeAt(1));
+
+//--------------------------------------------------------------
+
+// concat()
+
+// let firstName = "Krishna";
+// let lastName = " Nehru";
+
+// console.log(firstName.concat(lastName));
+
+//--------------------------------------------------------------
+
+// indexOf()
+
+// let myName = "Krishna";
+
+// console.log(myName.indexOf("h"));
+
+// console.log(myName.indexOf("a"));
+
+//--------------------------------------------------------------
+
+// lastIndexOf()
+
+// let myName = "Krishna harishchandra";
+
+// console.log(myName.lastIndexOf("h"));
+
+// console.log(myName.lastIndexOf("a"));
+
+//--------------------------------------------------------------
+
+// slice()
+
+// let myName = "Krishna kumar";
+
+// console.log(myName.slice(0, 7));
+
+// console.log(myName.slice(3));
+
+//--------------------------------------------------------------
+
+// substr() : deprecated method used for extracting substrings
+
+// let myName = "Krishna kumar";
+
+// console.log(myName.substr(0, 7));    
+
+// console.log(myName.substr(3));
+
+//--------------------------------------------------------------
+
+// substring()
+
+// let myName = "Krishna kumar";
+
+// console.log(myName.substring(0, 7));
+
+// console.log(myName.substring(3));
+
+//--------------------------------------------------------------
+
+// replace()
+
+// let myName = "Krishna kumar";
+
+// console.log(myName.replace("kumar", "Nehru"));
+
+//--------------------------------------------------------------
+
+// split()
+
+// let myName = "Krishna kumar";
+
+// console.log(myName.split(""));
+
+//--------------------------------------------------------------
+
+// toLowerCase()
+
+// let myName = "Krishna Kumar";
+
+// console.log(myName.toLowerCase());
+
+//--------------------------------------------------------------
+
+// toUpperCase()
+
+// let myName = "Krishna Kumar";
+
+// console.log(myName.toUpperCase());
+
+//--------------------------------------------------------------
+
+// trim()
+
+// let myName = "   Krishna Kumar  ";
+// let originalStringLength = myName.length;
+// let trimmedStringLength = myName.trim().length;
+
+// console.log("Original string length: " + originalStringLength);
+// console.log("Trimmed string length: " + trimmedStringLength);   
+
+//--------------------------------------------------------------
+
+//trimStart()
+
+// let myName = "   Krishna Kumar  ";
+// let originalStringLength = myName.length;
+// let trimmedStringLength = myName.trimStart().length;
+
+// console.log("Original string length: " + originalStringLength);
+// console.log("Trimmed string length: " + trimmedStringLength);
+
+//--------------------------------------------------------------
+
+//trimEnd()
+
+// let myName = "   Krishna Kumar   "
+// let originalStringLength = myName.length;
+// let trimmedStringLength = myName.trimEnd().length;
+
+// console.log("Original string length: " + originalStringLength);
+// console.log("Trimmed string length: " + trimmedStringLength);    
+
+//--------------------------------------------------------------
+
+// startsWith()
+
+// let myName = "Krishna Kumar";
+
+// console.log(myName.startsWith("Krishna"));
+
+//--------------------------------------------------------------
+
+// endsWith()
+
+// let myName = "Krishna Kumar";
+
+// console.log(myName.endsWith("Kumar"));
+
+//--------------------------------------------------------------
+
+// includes()
+
+// let myName = "Krishna Kumar";
+
+// console.log(myName.includes("Kum"));
+
+//--------------------------------------------------------------
+
+
+
